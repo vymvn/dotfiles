ef16df4a5a178b80d90105e6152377383d6d41e6
vymvn <vymvn6@gmail.com>
2023-12-27 00:30:11 +0400

    idk what that was

--- VSCodium/User/History/-5c6424d0/TvdS.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import pytesseract
-from PIL import Image
-
-cap = cv2.VideoCapture('flag.avi')
-
--- VSCodium/User/History/-5c6424d0/f3nl.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import pytesseract
-from PIL import Image
-
-video = cv2.VideoCapture('flag.avi')
-
